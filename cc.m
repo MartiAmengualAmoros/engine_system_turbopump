978ba0fd67e5116e5a445ff90672e15a60eaf7da
saue_ta <tasosstein@gmail.com>
2026-06-02 19:40:16 +0200

    tasos update

---
+++ cc.m
@@ -0,0 +1,37 @@
+﻿[inputs, properties_fuel, properties_oxidizer] = engine_inputs();
+
+% --- CH4 pump ---
+pump_CH4 = pump_efficiency_iteration( ...
+    inputs.m_dot_fuel, ...
+    inputs.delta_p_pump_LCH4, ...
+    properties_fuel.rho, 'Methane', ...
+    properties_fuel.p, properties_fuel.T);
+
+% --- LOx pump ---
+pump_LOx = pump_efficiency_iteration( ...
+    inputs.m_dot_oxidizer, ...
+    inputs.delta_p_pump_LOx, ...
+    properties_oxidizer.rho, 'Oxygen', ...
+    properties_oxidizer.p, properties_oxidizer.T);
+
+% --- Update efficiency back into inputs for downstream use ---
+inputs.eta_pump_LCH4 = pump_CH4.eta;
+inputs.eta_pump_LOx  = pump_LOx.eta;
+
+% Plot: max efficiency vs Omega_s for each pump
+Omega_s_CH4 = [pump_CH4.Omega_s];
+eta_CH4 = [pump_CH4.eta];
+[uniq_Omega_s_CH4, ~, ic_CH4] = unique(Omega_s_CH4);
+eta_max_CH4 = accumarray(ic_CH4(:), eta_CH4(:), [], @max);
+
+Omega_s_LOx = [pump_LOx.Omega_s];
+eta_LOx = [pump_LOx.eta];
+[uniq_Omega_s_LOx, ~, ic_LOx] = unique(Omega_s_LOx);
+eta_max_LOx = accumarray(ic_LOx(:), eta_LOx(:), [], @max);
+
+figure;
+plot(uniq_Omega_s_CH4, eta_max_CH4, 'b-', 'LineWidth', 2); hold on;
+plot(uniq_Omega_s_LOx, eta_max_LOx, 'r-', 'LineWidth', 2);
+xlabel('\Omega_s [-]'); ylabel('\eta [-]');
+legend('CH4 pump', 'LOx pump');
+title('Max Efficiency vs Specific Speed'); grid on;
--- sub_Pump_LOx.m
+++ sub_Pump_LOx.m
@@ -7,11 +7,17 @@ function [properties, key_values] = sub_Pump_LOx(inputs, properties)
     
     p_intermediate = properties.p + inputs.delta_p_pump_LOx;
     properties.p = p_intermediate - inputs.delta_p_partial;
-    
-    P_pump = (inputs.m_dot_oxidizer * inputs.delta_p_pump_LOx) / (inputs.eta_pump_LOx * properties.rho);
-    
-    properties.T = properties.T + P_pump * (1 - inputs.eta_pump_LOx) / properties.c_p;
-    
+
+    % Isentropic specific work for the pump
+    delta_h_isen = inputs.delta_p_pump_LOx / properties.rho;
+    % Actual specific work with pump efficiency
+    delta_h_actual = delta_h_isen / inputs.eta_pump_LOx;
+    P_pump = inputs.m_dot_oxidizer * delta_h_actual;
+
+    % Excess enthalpy due to inefficiency heats the fluid
+    delta_h_loss = delta_h_actual - delta_h_isen;
+    properties.T = properties.T + delta_h_loss / properties.c_p;
+
     properties.rho = py.CoolProp.CoolProp.PropsSI('D', 'P', properties.p, 'T', properties.T, 'Oxygen');
     properties.c_p = py.CoolProp.CoolProp.PropsSI('CPMASS', 'T', properties.T, 'P', properties.p, 'Oxygen');
 
